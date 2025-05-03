729ad34151ae63a1de689f7f031fa545c842af75
RosuGabriel <gabi.rosu2411@gmail.com>
2025-05-04 01:57:05 +0300

    Added data folder

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -6,5 +6,5 @@ BASE_DIR = Path(__file__).resolve().parent.parent + "/"
 
 DATA_DIR = BASE_DIR + "../data/"
 MOVIE_LENS_DIR = DATA_DIR + "ml-latest-small/"
-ADDITIONAL_DIR = DATA_DIR + "additional data/"
+ADDITIONAL_DIR = DATA_DIR + "additional_data/"
 MODELS_DIR = ADDITIONAL_DIR + "models/"

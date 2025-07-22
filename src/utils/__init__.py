8afea96b6481d9eae701f7b26fe043e2394c327d
RosuGabriel <gabi.rosu2411@gmail.com>
2025-07-22 17:09:29 +0300

    Small adjustments
    Moved SVD models together with AC models

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,10 +5,9 @@ from utils.paths import ADDITIONAL_DIR, BASE_DIR
 
 
 os.makedirs(ADDITIONAL_DIR, exist_ok=True)
-os.makedirs(ADDITIONAL_DIR / "models/", exist_ok=True)
 os.makedirs(BASE_DIR / "tmp/", exist_ok=True)
-os.makedirs(BASE_DIR / "tmp/actor critic", exist_ok=True)
-
+os.makedirs(BASE_DIR / "tmp/models checkpoint/", exist_ok=True)
+os.makedirs(BASE_DIR / "tmp/best models/", exist_ok=True)
 
 myRatingsPath = ADDITIONAL_DIR / "myRatings.csv"
 if not os.path.exists(myRatingsPath):
--- src/utils/paths.py
+++ src/utils/paths.py
@@ -7,4 +7,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "../data/"
 MOVIE_LENS_DIR = DATA_DIR / "ml-latest-small/"
 ADDITIONAL_DIR = DATA_DIR / "additional_data/"
-MODELS_DIR = ADDITIONAL_DIR / "models/"
+MODELS_DIR = BASE_DIR / "models/pretrained/"
